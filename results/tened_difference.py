4e20a0f79be4b96810afe4152a2097437301e0d0
Konstantin <konsti999@gmx.de>
2023-02-10 14:56:16 +0100

    figure creation update in tened_difference.py

--- results/tened_difference.py
+++ results/tened_difference.py
@@ -28,11 +28,11 @@ def load_value_from_checkpoint(epoch):
         overall_training_acc = checkpoint["training_accuracies"]
         overall_validation_acc = checkpoint["validation_accuracies"]
 
-        return overall_training_loss_shape[epoch - 5], overall_validation_loss_shape[epoch - 5], \
-               overall_training_loss_height[epoch - 5], overall_validation_loss_height[epoch - 5], \
-               overall_training_mse[epoch - 5], overall_validation_mse[epoch - 5], \
-               overall_training_ssim[epoch - 5], overall_validation_ssim[epoch - 5], \
-               overall_training_acc[epoch - 5], overall_validation_acc[epoch - 5]
+        return overall_training_loss_shape[epoch - 6], overall_validation_loss_shape[epoch - 6], \
+               overall_training_loss_height[epoch - 6], overall_validation_loss_height[epoch - 6], \
+               overall_training_mse[epoch - 6], overall_validation_mse[epoch - 6], \
+               overall_training_ssim[epoch - 6], overall_validation_ssim[epoch - 6], \
+               overall_training_acc[epoch - 6], overall_validation_acc[epoch - 6]
 
     else:
         print("No model found with epoch {}".format(
@@ -54,6 +54,15 @@ def load_graphs_from_checkpoint(epoch):
         overall_validation_mse = checkpoint['validation_mses']
         overall_validation_ssim = checkpoint['validation_ssims']
 
+        overall_training_f1 = checkpoint["training_f1s"]
+        overall_validation_f1 = checkpoint["validation_f1s"]
+
+        overall_training_recall = checkpoint["training_recalls"]
+        overall_validation_recall = checkpoint["validation_recalls"]
+
+        overall_training_precision = checkpoint["training_precisions"]
+        overall_validation_precision = checkpoint["validation_precisions"]
+
         overall_training_acc = checkpoint["training_accuracies"]
         overall_validation_acc = checkpoint["validation_accuracies"]
 
@@ -61,7 +70,10 @@ def load_graphs_from_checkpoint(epoch):
                overall_training_loss_height, overall_validation_loss_height, \
                overall_training_mse, overall_validation_mse, \
                overall_training_ssim, overall_validation_ssim, \
-               overall_training_acc, overall_validation_acc
+               overall_training_acc, overall_validation_acc, \
+               overall_training_f1, overall_validation_f1, \
+               overall_training_recall, overall_validation_recall, \
+               overall_training_precision, overall_validation_precision
 
     else:
         print("No model found with epoch {}".format(
@@ -125,39 +137,57 @@ def find_max_epoch(path, v):
 
 
 if __name__ == '__main__':
-    plt.figure()
+    fig, axs = plt.subplots(1, 2, figsize=(15, 6), sharex=True)
+    fig.text(0.5, 0.02, 'Amount of epochs trained', ha='center', size=16.9)
+    fig.text(0.07, 0.5, 'Score [0, 1]', va='center', rotation='vertical', size=16.9)
 
-    # for m in range(1, 8):
-    #     current = "B:/projects/PycharmProjects/ScientificWriting/output/best_of_models/results_single_BCEWithLogitsLoss_L1Loss_Adam_Adam_UNET_SHAPE_UNET_HEIGHT_1e-05_1e-05_below" + str(m)
-    #
-    #     os.chdir(current)
-    #
-    #     tr_loss_shapes, val_loss_shapes, tr_loss_heights, val_loss_heights, tr_mses, val_mses, \
-    #     tr_ssims, val_ssims, tr_accs, val_accs = find_max_epoch(current, False)
-    #
-    #     plt.plot(tr_ssims, c='blue')
-    #
-    # current = "B:/projects/PycharmProjects/ScientificWriting/output/best_of_models/results_combined_L1Loss_Adam_UNET_0.001"
-    # os.chdir(current)
-    #
-    # tr_loss_heights, val_loss_heights, tr_mses, val_mses, tr_ssims, val_ssims = find_max_epoch_combined(current)
-    #
-    # plt.plot(tr_ssims, c='red')
-    #
-    # plt.legend(loc="lower right", fontsize=10)
-    # plt.show()
-
-    points = []
+    axs[0].title.set_text('F1-Score')
+    axs[1].title.set_text('Accuracy')
 
     for m in range(1, 11):
-        current = "B:/projects/PycharmProjects/ScientificWriting/output/best_of_models/results_single_BCEWithLogitsLoss_L1Loss_Adam_Adam_UNET_SHAPE_UNET_HEIGHT_1e-05_1e-05_below" + str(m)
+        col = (np.random.random(), np.random.random(), np.random.random())
+        current = "C:/Users/Konstantin/PycharmProjects/ScientificWriting/output/best_of_models/results_single_BCEWithLogitsLoss_L1Loss_Adam_Adam_UNET_SHAPE_UNET_HEIGHT_1e-05_1e-05_below" + str(m)
 
         os.chdir(current)
 
-        tr_loss_shape, val_loss_shape, tr_loss_height, val_loss_height, tr_mse, val_mse, \
-        tr_ssim, val_ssim, tr_acc, val_acc = find_max_epoch(current, True)
+        tr_loss_shapes, val_loss_shapes, tr_loss_heights, val_loss_heights, tr_mses, val_mses, \
+        tr_ssims, val_ssims, tr_accs, val_accs, tr_f1s, val_f1s, tr_recs, val_recs, tr_precs, val_precs = find_max_epoch(current, False)
 
-        points.append(val_ssim)
+        axs[0].plot(
+            2 * np.divide(np.multiply(tr_precs, tr_recs), (np.add(tr_precs, tr_recs))),
+            c=col,
+            label=chr(964) + " < " + str(m) + "m"
+        )
+        axs[0].legend()
+        axs[1].plot(
+            tr_accs,
+            c=col,
+            label=chr(964) + " < " + str(m) + "m"
+        )
+        axs[1].legend()
 
-    plt.plot(points)
-    plt.show()
+
+    # current = "B:/projects/PycharmProjects/ScientificWriting/output/best_of_models/results_combined_L1Loss_Adam_UNET_1e-05"
+    # os.chdir(current)
+
+    # tr_loss_heights, val_loss_heights, tr_mses, val_mses, tr_ssims, val_ssims = find_max_epoch_combined(current)
+    # plt.plot(tr_ssims, c='red')
+
+    # plt.legend(loc="lower right", fontsize=10)
+    plt.savefig("C:/Users/Konstantin/Desktop/scientific_figures/f1saccs.png", dpi=600)
+    plt.close(fig)
+
+    # points = []
+    #
+    # for m in range(1, 11):
+    #     current = "C:/Users/Konstantin/PycharmProjects/ScientificWriting/output/best_of_models/results_single_BCEWithLogitsLoss_L1Loss_Adam_Adam_UNET_SHAPE_UNET_HEIGHT_1e-05_1e-05_below" + str(m)
+    #
+    #     os.chdir(current)
+    #
+    #     tr_loss_shape, val_loss_shape, tr_loss_height, val_loss_height, tr_mse, val_mse, \
+    #     tr_ssim, val_ssim, tr_acc, val_acc = find_max_epoch(current, True)
+    #
+    #     points.append(val_loss_height)
+    #
+    # plt.plot(points)
+    # plt.show()
